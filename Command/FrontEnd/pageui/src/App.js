85ea46e9f959703cd99a68d613f4f4fe05b30c42
Aeroking21 <55886814+Aeroking21@users.noreply.github.com>
2022-05-30 12:51:10 +0100

    Made basic battery with just numbers

--- Command/FrontEnd/pageui/src/App.js
+++ Command/FrontEnd/pageui/src/App.js
@@ -15,7 +15,6 @@ function App() {
         <Route path='/' element={<About /> } />
 
         <Route path='/battery' element={<Battery /> } />
-        
 
       </Routes>
 
--- Command/FrontEnd/pageui/src/Components/Battery.js
+++ Command/FrontEnd/pageui/src/Components/Battery.js
@@ -1,9 +1,25 @@
-
+import { useState } from "react";
+import logo from './logo192.png'
 
 const Battery = () => {
+  const [batteryLevel, setBatteryLevel] = useState(0);
+
+
+  const whenClicked = () =>{ 
+    console.log(batteryLevel);
+    return setBatteryLevel(Math.floor(Math.random() * 100));
+  };
+
   return (
-    <div>Battery</div>
+
+    <div className="d-flex flex-row justify-content-evenly align-items-center" style={{minHeight: "93vh", maxHeight:"100vh"}} >
+      <div className="p-2 mr-5" style={{ border:"4px solid green"}} > {`${batteryLevel} %`}  </div>
+      <img src={logo} className="App-logo" alt="logo" />
+     <button type="button" className="btn btn-primary btn-lg" onClick={whenClicked} > Check Health </button>
+    </div>
+
   )
 }
 
 export default Battery
+
