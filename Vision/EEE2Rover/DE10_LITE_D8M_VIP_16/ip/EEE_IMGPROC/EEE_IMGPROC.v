cd0c93a99253dc80451f6362714374e759c9b294
Shaheen <Flamebull5@gmail.com>
2022-06-10 02:31:13 +0100

    Saving just in case

--- Vision/EEE2Rover/DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/EEE_IMGPROC.v
+++ Vision/EEE2Rover/DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/EEE_IMGPROC.v
@@ -27,10 +27,16 @@ module EEE_IMGPROC(
 	
 	// conduit
 	mode,
-	thresholdsHue, //Posterization thresholds to be accessed as MM by NIOS2
-	numThresholds, //Thresholds to use ^^
-	thresholdSat,
-	thresholdVal
+
+	//VSPI interface
+	avalon_master_write,
+	avalon_master_writedata,
+	avalon_master_read,
+	avalon_master_readdata,
+	avalon_master_address,
+	avalon_master_byteenable,
+	avalon_master_chipselect,
+	avalon_master_waitrequest
 );
 
 
@@ -63,10 +69,16 @@ output								source_eop;
 
 // conduit export
 input                         mode; //externally connected to switch 0
-input [143:0] thresholdsHue; //Posterization thresholds to be accessed as MM by NIOS2 (16 x 9 bit coefficients)
-input [3:0] numThresholds; //Thresholds to use ^^
-input [7:0] thresholdSat;
-input [7:0] thresholdVal;
+
+//row data buffer
+output							avalon_master_write;
+output	[31:0]				avalon_master_writedata;
+output avalon_master_read;
+input				[31:0]	avalon_master_readdata;
+output	[15:0]					avalon_master_address;
+output [3:0] avalon_master_byteenable;
+output avalon_master_chipselect;
+input avalon_master_waitrequest;
 
 ////////////////////////////////////////////////////////////////////////
 //
@@ -79,119 +91,324 @@ parameter BB_COL_DEFAULT = 24'h00ff00;
 
 wire [7:0]   red, green, blue, gray;
 wire [7:0]   red_out, green_out, blue_out;
-
 wire         sop, eop, in_valid, out_ready;
+reg [23:0] colourOutput;
+//assign {red_out, green_out, blue_out} = (mode & ~sop & packet_video)? {newRed, newGreen, newBlue} : {red,green,blue};
+assign {red_out, green_out, blue_out} = (mode & ~sop & packet_video)? colourOutput : {red,green,blue};
 //HSV calculations
-wire [143:0] tempThresholdsHue;
-wire [15:0] Saturation;
 wire [8:0] Hue;
-wire [7:0] maxVal, minVal, Value, finalSat, finalVal, tempThresholdSat, tempThresholdVal;
-wire [3:0] tempNumThresholds;
+wire [15:0] Saturation;
+wire [7:0] maxVal, minVal, Value, delta;
 wire isRedMax, isGreenMax, isBlueMax;
 wire isRedMin, isGreenMin, isBlueMin;
-wire satThresholdMet, valThresholdMet;
 
+//reg [24:0] dataBuffer[639:0][2:0] ;//This contains the data of the last three rows in the image in HSV *(9 + 8 + 8 = 25)
 reg [23:0] runningValueTotal;
-reg [15:0] zPartial, zPartial2, zPartial3;
-reg [13:0] HRed, HGreen, HBlue;
-reg [8:0] finalHue, zHSV, zmSum;
-reg [7:0] newRed, newGreen, newBlue, averageVal, mHSV, delta;
-
-assign tempNumThresholds = 4'd5;
-assign tempThresholdSat = 8'd128;
-assign tempThresholdVal = 8'd128;
-assign tempThresholdsHue = {9'd0, 9'd80, 9'd180, 9'd250, 9'd310, 99'd0}; //Red, green, cyan, blue, pink
+wire [15:0] zPartial, zPartial2, zPartial3, mHSV;
+wire [13:0] HRed, HGreen, HBlue;
+reg [8:0] finalHue, zmSum;
+wire [8:0] outputHue;
+wire [7:0] outputSat, outputVal, newRed, newGreen, newBlue;
+reg [7:0] finalSat, finalVal;
+reg [10:0] x, y;
 ////////////////////////////////////////////////////////////////////////
 
 assign gray = green[7:1] + red[7:2] + blue[7:2]; //Grey = green/2 + red/4 + blue/4 (average of all colours to produce grayscale)
 //Pixels enter sequentially
+
 //Convert to HSV:
 //Max value
-assign isRedMax = (red > blue) & (red > green) ? 1 : 0; //One bit
-assign isGreenMax = (green > blue) & (green > red) ? 1 : 0;
-assign isBlueMax = (blue > red) & (blue > green) ? 1 : 0;
+assign isRedMax = ((red > blue) && (red > green)) ? 1 : 0; //One bit
+assign isGreenMax = ((green > blue) && (green > red)) ? 1 : 0;
+assign isBlueMax = ((blue > red) && (blue > green)) ? 1 : 0;
 assign maxVal = isRedMax ? red : (isBlueMax ? blue : green);
-
 //Min val
-assign isRedMin = (red < blue) & (red < green) ? 1 : 0; //One bit
-assign isGreenMin = (green < blue) & (green < red) ? 1 : 0;
-assign isBlueMin = (blue < red) & (blue < green) ? 1 : 0;
+assign isRedMin = ((red < blue) && (red < green)) ? 1 : 0; //One bit
+assign isGreenMin = ((green < blue) && (green < red)) ? 1 : 0;
+assign isBlueMin = ((blue < red) && (blue < green)) ? 1 : 0;
 assign minVal = isRedMin ? red : (isBlueMin ? blue : green);
-////
-assign Hue = isRedMax ? HRed : (isGreenMax ? HGreen : HBlue); //(0-360) 9 bits
-assign Saturation = (delta << 8)/maxVal; //(0-255) //8Bits, max 16 bits
+//Intermediate values
+assign delta = maxVal-minVal; //8 bits
+//RGB to HSV calculations - Assume correct
+// assign HRed = ((delta == 0) || (((green-blue) % 6) == 0) || (((blue-green) % 6) == 0)) ? 14'd0 : 
+// ((green < blue) ? (60*(((green-blue)/delta) % 6)) : (60*(6-((blue-green)/delta) % 6))); //Max 14 bits
+// assign HGreen = (delta == 0) ? 14'd0 :
+// ((blue > red) ? (((60 *(blue-red))/delta) + 120) : (120 - ((60 *(red-blue))/delta)));
+// assign HBlue = (delta == 0) ? 14'd0 : 
+// ((red > green) ? (((60 *(red-green))/delta) + 240) : (240 - ((60 *(green-red))/delta)));
+assign Hue = (delta == 0) ? 14'd0 :
+((isRedMax && isBlueMin) ? ((60*(green-blue))/delta) :
+((isGreenMax && isBlueMin) ? (60+((60*(red-blue))/delta)) :
+((isGreenMax && isRedMin) ? (120+((60*(blue-red))/delta)) :
+((isBlueMax && isRedMin) ? (180+((60*(green-red))/delta)) :
+((isBlueMax && isGreenMin) ? (240+((60*(red-green))/delta)) :
+(300+((60*(blue-green))/delta)))))));
+
+//assign Hue = isRedMax ? HRed[8:0] : 
+//(isBlueMax ? HBlue[8:0] : HGreen[8:0]); //(0-360) 9 bits
+assign Saturation = (maxVal == 0) ? 8'd0 : ((delta << 8)/maxVal); //(0-255) //8Bits, max 16 bits
 assign Value = maxVal; //(0-255)
+//Convert back to RGB - might not be needed
+// assign outputHue = (Hue % 360);
+// assign outputSat = Saturation[7:0];
+// assign outputVal = Value;
+// assign mHSV = (outputVal - ((outputSat*outputVal)>>8)); //8bits
+// assign zPartial = (((outputHue << 7)/60) % 256); //Ranges from 0 to 255
+// assign zPartial2 = (zPartial > 127) ? (zPartial-128) : (127-zPartial); //Must be positive : ranges from 0 to 127
+// assign zPartial3 = ((outputSat*zPartial2[7:0]) >> 7);
+// assign zmSum = (outputSat > zPartial3) ? ((outputSat-zPartial3)+mHSV) : (mHSV-(zPartial3-outputSat));
+// //
+// assign newRed = (outputHue < 60) ? outputVal : 
+// ((outputHue < 120) ? zmSum[7:0] :  
+// ((outputHue < 240) ? mHSV[7:0] :
+// ((outputHue < 300) ? zmSum[7:0] : outputVal )));
 
-//Thresholds - might need to encase in clocked logic
-assign satThresholdMet = (tempThresholdSat < Saturation) ? 1 : 0;
-assign valThresholdMet = (tempThresholdVal < Saturation) ? 1 : 0;
+// assign newGreen = (outputHue < 60) ? zmSum[7:0]: 
+// ((outputHue < 180) ? outputVal :
+// ((outputHue < 240) ? zmSum[7:0] : mHSV[7:0]));
 
-//Final values for S and V
-assign finalSat = satThresholdMet ? 255 : Saturation;
-assign finalVal = valThresholdMet ? 255 : Value;
+// assign newBlue = (outputHue < 120) ? mHSV[7:0] : 
+// ((outputHue < 180) ?  zmSum[7:0] : 
+// ((outputHue < 300) ?  outputVal: zmSum[7:0]));
 
-assign {red_out, green_out, blue_out} = (mode & ~sop & packet_video)? {newRed, newGreen, newBlue} : {red,green,blue};
-
+//////////////////// Threshold sections
+wire red_detect, blue_detect, green_detect, orange_detect, pink_detect, gray_detect, blueOrGreen;
+////////////////////
+//assign red_detect = ((compHue > 330) || (compHue < 35)) ? ((compSat > 150) ? ((compVal > 60) ? 1 : 0) : 0) : 0;
+// assign red_detect = ((Hue > 330) || (Hue < 25)) ? ((Saturation > 155) ? ((Value > 80) ? 1 : 0) : 0) : 0;X
+// assign blue_detect = ((Hue > 110) && (Hue < 185)) ? ((Saturation < 128) ? ((Value > 25) ? 1 : 0) : 0) : 0;
+// assign green_detect = ((Hue > 110) && (Hue < 185)) ? ((Saturation > 115 && Saturation < 204) ? ((Value > 25) ? 1 : 0) : 0) : 0;
+// assign pink_detect = ((Hue > 330) || (Hue < 35)) ? ((Saturation > 135 && Saturation < 195) ? ((Value > 100) ? 1 : 0) : 0) : 0;
+// assign gray_detect = 0;
+assign red_detect =  (Hue < 40) ? ((Saturation > 185) ? ((Value > 105) ? 1 : 0) : 0) : 0;
+assign green_detect =  (Hue > 45 && Hue < 130) ? ((Saturation > 35 && Saturation < 200) ? ((Value > 25) ? 1 : 0) : 0) : 0;
+assign blue_detect =  (Hue > 40 && Hue < 100) ? ((Saturation < 155) ? ((Value > 50) ? 1 : 0) : 0) : 0;
+assign gray_detect = 0;
+assign pink_detect = 0;
+assign blueOrGreen = green_detect && blue_detect;
+////////////////////
+// assign newRed = (red_detect || pink_detect || gray_detect) ? 8'd255 : gray;
+// assign newGreen = (green_detect || gray_detect) ? 8'd255 : gray;
+// assign newBlue = (blue_detect || pink_detect) ? 8'd255 : gray;
+//Red = 0, Green = 1, Blue = 2, 3 = Orange, 4 = Pink, 5 = Gray
+wire detectionArray [5:0];
+//assign detectionArray = {red_detect, green_detect, blue_detect, orange_detect, pink_detect, gray_detect};
+assign detectionArray[0] = red_detect;
+assign detectionArray[1] = green_detect;
+assign detectionArray[2] = blue_detect;
+assign detectionArray[3] = orange_detect;
+assign detectionArray[4] = pink_detect;
+assign detectionArray[5] = gray_detect;
+reg [5:0] pixelBuffer [6:0]; //Shift reg of 7 bits
+reg [5:0] xMin [9:0]; //640
+reg [5:0] xMax [9:0]; //640
+reg [5:0] yMin [8:0]; //480 may not be required
+reg [5:0] yMax [8:0]; //480 may not be required
+wire [5:0] xDistanceVector [9:0]; //Distance between xMin and xMax
+wire [5:0] yDistanceVector [8:0]; //Distance between yMin and yMax may not be required
+//assign xDistanceVector = {((xMin[0] < xMax[0]) ? xMax[0]-xMin[0] : 0), ((xMin[1] < xMax[1]) ? xMax[1]-xMin[1] : 0), ((xMin[2] < xMax[2]) ? xMax[2]-xMin[2] : 0), ((xMin[3] < xMax[3]) ? xMax[3]-xMin[3] : 0), ((xMin[4] < xMax[4]) ? xMax[4]-xMin[4] : 0), ((xMin[5] < xMax[5]) ? xMax[5]-xMin[5] : 0)};
+assign xDistanceVector[0] = (xMin[0] < xMax[0]) ? xMax[0]-xMin[0] : 0;
+assign xDistanceVector[1] = (xMin[1] < xMax[1]) ? xMax[1]-xMin[1] : 0;
+assign xDistanceVector[2] = (xMin[2] < xMax[2]) ? xMax[2]-xMin[2] : 0;
+assign xDistanceVector[3] = (xMin[3] < xMax[3]) ? xMax[3]-xMin[3] : 0;
+assign xDistanceVector[4] = (xMin[4] < xMax[4]) ? xMax[4]-xMin[4] : 0;
+assign xDistanceVector[5] = (xMin[5] < xMax[5]) ? xMax[5]-xMin[5] : 0;
+//
+assign yDistanceVector[0] = (yMin[0] < yMax[0]) ? yMax[0]-yMin[0] : 0;
+assign yDistanceVector[1] = (yMin[1] < yMax[1]) ? yMax[1]-yMin[1] : 0;
+assign yDistanceVector[2] = (yMin[2] < yMax[2]) ? yMax[2]-yMin[2] : 0;
+assign yDistanceVector[3] = (yMin[3] < yMax[3]) ? yMax[3]-yMin[3] : 0;
+assign yDistanceVector[4] = (yMin[4] < yMax[4]) ? yMax[4]-yMin[4] : 0;
+assign yDistanceVector[5] = (yMin[5] < yMax[5]) ? yMax[5]-yMin[5] : 0;
+//assign yDistanceVector = {((yMin[0] < yMax[0]) ? yMax[0]-yMin[0] : 0), ((yMin[1] < yMax[1]) ? yMax[1]-yMin[1] : 0), ((yMin[2] < yMax[2]) ? yMax[2]-yMin[2] : 0), ((yMin[3] < yMax[3]) ? yMax[3]-yMin[3] : 0), ((yMin[4] < yMax[4]) ? yMax[4]-yMin[4] : 0), ((yMin[5] < yMax[5]) ? yMax[5]-yMin[5] : 0)};
+wire [5:0] colourCodes [23:0]; //Holds output colour codes for all balls
+assign colourCodes[0] = {24'hff0000, 24'h00ff00, 24'h0000ff, 24'hff8000, 24'hff00ff, 24'hffffff};
+reg [2:0] tempCount;
+//New implementation using detection as mode filter
 always @(posedge clk) begin
-	//HSV calculations
-	delta <= maxVal-minVal; //8 bits
-	HRed <= ((60 * (green-blue))/delta) % 6; //Max 14 bits
-	HGreen <= (((60 *(blue-red))/delta) + 120) % 255;
-	HBlue <= (((60 *(red-green))/delta) + 240) % 255;
-	//Convert back to RGB
-	mHSV <= finalVal - finalSat; //8bits
-	zPartial <= (((finalHue << 7)/60) % 256) - 128; //Ranges from -128 to 127
-	zPartial2 <= zPartial[15] ? -zPartial : zPartial; //Must be positive : ranges from 0 to 127
-	zPartial3 <= ((finalSat*zPartial2[7:0]) >> 7);
-	zHSV <= finalSat - zPartial3; //Obtain final value for z (8 bits)
-	zmSum <= zHSV + mHSV;
-	//Set finalHue to nearest threshold value
-	if (satThresholdMet & valThresholdMet) begin
-		integer i;
-		for(i = 1; i < tempNumThresholds; i = i + 1) begin
-			if (tempThresholdsHue[9*(i-1)+:9] > Hue) begin
-				if (i > 1) begin
-					finalHue <= tempThresholdsHue[9*(i-2)+:9];
-				end else begin
-					finalHue <= 9'b0;
-				end
+	integer i;
+	integer j;
+	colourOutput = {gray, gray, gray};
+	//Reset all at start of frame
+	if (sop) begin
+		for(i = 0; i < 6; i = i + 1) begin
+			pixelBuffer[i] = 0;
+			xMin[i] = 0;
+			yMin[i] = 0;
+		end
+	end
+	//Reset buffer at end of line
+	if ((in_valid) && (x == IMAGE_W-1)) begin
+		for(i = 0; i < 6; i = i + 1) begin
+			pixelBuffer[i] = 0;
+		end
+	end
+	//If the current coordinate is an xMin, xMax, yMin or yMax of another ball, colour accordingly
+	for(j = 0; i < 6; i = i + 1) begin
+		if ((xMin[i] == x) || (xMax[i] == x) || (yMin[i] == y) || (yMax[i] == y)) begin
+			//The difference between the two coordinates must be at least 20 to be considered
+			colourOutput = ((xDistanceVector[i] > 20) || (yDistanceVector[i] > 20)) ? colourCodes[i] : gray;  
+		end
+	end
+	for(i = 0; i < 6; i = i + 1) begin
+		//If a colour pixel was detected, add to buffers for that colour
+		tempCount = 0;
+		if(detectionArray[i] == 1) begin
+			pixelBuffer[i][0] = 1;
+		end
+		//Count how many 1s in buffer, if at least 4, consider the pixel detected (mode filtering)
+		tempCount = 0;
+		for(j = 0; i < 7; i = i + 1) begin
+			if(pixelBuffer[i][j] == 1) begin
+				tempCount = tempCount + 1 ;
 			end
 		end
-		if (finalHue < 60) begin
-			newRed <= finalVal;
-			newGreen <= zmSum;
-			newBlue <= mHSV;
-		end	else if (finalHue < 120) begin
-			newRed <= zmSum;
-			newGreen <= finalVal;
-			newBlue <= mHSV;
-		end	else if (finalHue < 180) begin
-			newRed <= mHSV;
-			newGreen <= finalVal;
-			newBlue <= zmSum;
-		end	else if (finalHue < 240) begin
-			newRed <= mHSV;
-			newGreen <= zmSum;
-			newBlue <= finalVal;
-		end	else if (finalHue < 300) begin
-			newRed <= zmSum;
-			newGreen <= mHSV;
-			newBlue <= finalVal;
-		end	else if (finalHue < 360) begin
-			newRed <= finalVal;
-			newGreen <= mHSV;
-			newBlue <= zmSum;
-		end
-	//If pixels do not meet the required levels of saturation and value, reduce them to grayscale
-	end else begin
-		newRed <= gray;
-		newGreen <= gray;
-		newBlue <= gray;
+		//Mode filtering
+		if (tempCount > 3) begin
+			xMin[i] = xMin[i] > x ? x : xMin[i];
+			xMax[i] = xMax[i] < x ? x : xMax[i];
+			yMin[i] = yMin[i] > y ? y : yMin[i];
+			yMax[i] = yMax[i] < y ? y : yMax[i];
+			//Colour output depending on the mode filtering
+			colourOutput = colourCodes[i];
+		end 
+	end
+	//Shift all buffers by 1 (shift register)
+	for(i = 0; i < 6; i = i + 1) begin
+		pixelBuffer[i] = pixelBuffer[i] << 1;
 	end
 end
 
+
+reg [8:0] hueDataBuffer [4:0]; //Store the last 5 pixel Hues
+reg [8:0] hueStack [4:0];
+reg [7:0] saturationDataBuffer [4:0]; 
+reg [7:0] saturationStack [4:0];
+reg [7:0] valueDataBuffer [4:0]; 
+reg [7:0] valueStack [4:0];
+reg [8:0] tempRegHue, compHue;
+reg [7:0] tempRegSat, tempRegVal, compSat, compVal;
+reg hueReplaced, satReplaced, valReplaced;
+localparam HueSteps = 16;
+localparam SatSteps = 4;
+localparam ValSteps = 2; 
+localparam HueIncr = 22;//360/HueSteps;
+localparam SatIncr = 63; //255/SatSteps;
+localparam ValIncr = 127; //255/ValSteps;
+
+
+//Saving to data buffer
+always @(*) begin
+	integer i;
+	//Reset buffers at start of frame
+	// if (sop) begin
+	// 	for(i = 0; i < 5; i = i + 1) begin
+	// 		hueDataBuffer[i] = 9'd0;
+	// 		saturationDataBuffer[i] = 8'd0;
+	// 		valueDataBuffer[i] = 8'd0;
+	// 		hueStack[i] = 9'd0;
+	// 		saturationStack[i] = 8'd0;
+	// 		valueStack[i] = 8'd0;
+	// 	end
+	// end
+	//Posterize/quantise values
+	//If inbetween these two bounds, set it to whichever it is closer to
+	// for(i = 0; i < HueSteps; i = i + 1) begin
+	// 	//Hue
+	// 	if ((Hue > i*HueIncr) && (Hue < (i+1)*HueIncr)) begin
+	// 		if (Hue > (i*HueIncr)+HueIncr/2) begin
+	// 			finalHue = (i+1)*HueIncr;
+	// 		end else begin
+	// 			finalHue = i*HueIncr;
+	// 		end
+	// 	end
+	// 	//Sat
+	// 	if(i < SatSteps) begin
+	// 		if ((Saturation > i*SatIncr) && (Saturation < (i+1)*SatIncr)) begin
+	// 			if (Saturation > (i*SatIncr)+SatIncr/2) begin
+	// 				finalSat = (i+1)*SatIncr;
+	// 			end else begin
+	// 				finalSat = i*SatIncr;
+	// 			end
+	// 		end
+	// 	end
+	// 	//Val
+	// 	if (i < ValSteps) begin
+	// 		if ((Value > i*ValIncr) && (Value < (i+1)*ValIncr)) begin
+	// 			if (Value > (i*ValIncr)+ValIncr/2) begin
+	// 				finalVal = (i+1)*ValIncr;
+	// 			end else begin
+	// 				finalVal = i*ValIncr;
+	// 			end
+	// 		end
+	// 	end
+	// end
+	//Save data to stacks and buffers
+	hueReplaced = 0;
+	satReplaced = 0;
+	valReplaced = 0;
+	finalHue = Hue;
+	finalSat = Saturation[7:0];
+	finalVal = Value;
+	for(i = 0; i < 5; i = i + 1) begin
+		if ((!hueReplaced) && (hueDataBuffer[i] == hueStack[x % 5])) begin
+			hueDataBuffer[i] = finalHue;
+			hueReplaced = 1;
+		end
+		if ((!satReplaced) && (saturationDataBuffer[i] == saturationStack[x % 5])) begin
+			saturationDataBuffer[i] = finalSat;
+			satReplaced = 1;
+		end
+		if ((!valReplaced) && (valueDataBuffer[i] == valueStack[x % 5])) begin
+			valueDataBuffer[i] = finalVal;
+			valReplaced = 1;
+		end
+	end 
+	hueStack[x % 5] = finalHue;
+	saturationStack[x % 5] = finalSat;
+	valueStack[x % 5] = finalVal;
+	for(i = 0; i < 4; i = i + 1) begin
+		//Do one sweep of the arrays rightward and leftward to ensure they are sorted
+		if(hueDataBuffer[i] > hueDataBuffer[i+1]) begin
+			tempRegHue = hueDataBuffer[i];
+			hueDataBuffer[i] = hueDataBuffer[i+1];
+			hueDataBuffer[i+1] = tempRegHue;
+		end
+		if(saturationDataBuffer[i] > saturationDataBuffer[i+1]) begin
+			tempRegSat = saturationDataBuffer[i];
+			saturationDataBuffer[i] = saturationDataBuffer[i+1];
+			saturationDataBuffer[i+1] = tempRegSat;
+		end
+		if(valueDataBuffer[i] > valueDataBuffer[i+1]) begin
+			tempRegVal = valueDataBuffer[i];
+			valueDataBuffer[i] = valueDataBuffer[i+1];
+			valueDataBuffer[i+1] = tempRegVal;
+		end
+	end
+	for(i = 4; i > 0; i = i - 1) begin
+		//Do one sweep of the arrays rightward and leftward to ensure they are sorted
+		if(hueDataBuffer[i] < hueDataBuffer[i-1]) begin
+			tempRegHue = hueDataBuffer[i];
+			hueDataBuffer[i] = hueDataBuffer[i-1];
+			hueDataBuffer[i-1] = tempRegHue;
+		end
+		if(saturationDataBuffer[i] < saturationDataBuffer[i-1]) begin
+			tempRegSat = saturationDataBuffer[i];
+			saturationDataBuffer[i] = saturationDataBuffer[i-1];
+			saturationDataBuffer[i-1] = tempRegSat;
+		end
+		if(valueDataBuffer[i] < valueDataBuffer[i-1]) begin
+			tempRegVal = valueDataBuffer[i];
+			valueDataBuffer[i] = valueDataBuffer[i-1];
+			valueDataBuffer[i-1] = tempRegVal;
+		end
+	end
+	//Data is now sorted across all HSV arrays, middle value is median, use threshold detection on that
+	compHue = hueDataBuffer[2];
+	compSat = saturationDataBuffer[2];
+	compVal = valueDataBuffer[2];
+end
 //Count valid pixels to tget the image coordinates. Reset and detect packet type on Start of Packet.
-reg [10:0] x, y;
 reg packet_video;
 always@(posedge clk) begin
 	if (sop) begin
@@ -223,7 +440,6 @@ always@(posedge clk) begin
 		//Start message writer FSM once every MSG_INTERVAL frames, if there is room in the FIFO
 		frame_count <= frame_count - 1;
 		//Calculate average value of the frame
-		averageVal <=  runningValueTotal >> 16;
 		if (frame_count == 0 && msg_buf_size < MESSAGE_BUF_MAX - 3) begin
 			msg_state <= 2'b01;
 			frame_count <= MSG_INTERVAL-1;
