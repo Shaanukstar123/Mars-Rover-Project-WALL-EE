dfb8996b52fdf3e012666dc6e496efc0567aa2a5
Shaanukstar123 <shaanukag0@gmail.com>
2022-06-11 22:22:14 +0100

    counter made to check MQTT delay

--- Command/BackEnd/MQTT/pub.js
+++ Command/BackEnd/MQTT/pub.js
@@ -1,9 +1,13 @@
 const mqtt = require("mqtt");
 var client = mqtt.connect('mqtt://35.176.71.115');
+var i = 0;
 
 client.on("connect",function(){
     setInterval(function(){
+        i = i+1;
+
         var random = Math.random()* 50;
+        random = i;
         console.log(random); //random value to publish (until I get some actual data)
 
         //topic name is 'epic'
