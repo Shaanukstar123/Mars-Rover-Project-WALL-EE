c9979c17741b80528e492718ed467f533ef6717f
Shaanukstar123 <shaanukag0@gmail.com>
2022-06-17 14:47:40 +0100

    MongoDB works with backend

--- Command/BackEnd/firstApp/server.js
+++ Command/BackEnd/firstApp/server.js
@@ -7,6 +7,10 @@ app = express();
 const mqtt = require("mqtt");
 var client = mqtt.connect('mqtt://35.176.71.115');
 
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //JSON variables
 var location ={
   xcoord:0,
@@ -16,21 +20,71 @@ var location ={
 var battery = {percentage: 0};
 var direction = {direction: 0};
 
-// MongoDB URL connection string
-const dbURI = 'mongodb+srv://shaanu:<password>@cluster0.k6p9c.mongodb.net/?retryWrites=true&w=majority';
+// MongoDB Schemas and Models
+const roverSchema = new mongoose.Schema({
+  id: Number,
+  xcoord: Number,
+  ycoord: Number,
+  obstacle: Number 
+});
 
-app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const Rover = mongoose.model('coordinates', roverSchema);
+
+const batterySchema = new mongoose.Schema({
+  id: Number,
+  percentage: Number,
+});
+
+const Battery = mongoose.model("batterylevels", batterySchema);
+
+const alienSchema = new mongoose.Schema({
+  color: String,
+  xcoord: Number,
+  ycoord: Number,
+});
+
+const Alien = mongoose.model("obstacles", alienSchema);
+
+
 
 //Connection  String
-// let connection = 'mongodb+srv://shaanu:<psswd>@cornflakes.k6p9c.mongodb.net/sample-db?retryWrites=true&w=majority';
-// mongoose.connect(connection, { useNewUrlParse: true, UseUnifiedTopology: true})
-//   .then(app.listen(8080, () => {
-//     console.log('Listening on port 8080');
-//   }))
-//   .catch((err) => console.log(err));
+let connection = "mongodb+srv://temp:marsrover@cornflakes.k6p9c.mongodb.net/sample-db?retryWrites=true&w=majority";
 
+// Making sure app doesn't lisen before database is connected
+mongoose.connect(connection, { useNewUrlParser: true, UseUnifiedTopology: true})
+  .then(app.listen(8080, () => {
+    console.log('Listening on port 8080');
+
+    Battery.deleteMany({})
+      .then(console.log("Deleted Battery Collection"))
+      .catch((err) => console.log(err))
+    ;
+
+
+    Rover.deleteMany({})
+      .then(console.log("Deleted Rover Collection"))
+      .catch((err) => console.log(err))
+    ;
+
+    new Battery({
+      id: 732,
+      percentage: 999
+    }).save();
+
+    new Rover({
+      id: 732,
+      xcoord: 250,
+      ycoord: 250,
+      obstacle: 1
+    }).save();
+
+
+  }))
+
+  .catch((err) => console.log(err))
+;
+
+//MQTT callback functions
 
 client.on('connect', function() { //MQTT subscribe
   client.subscribe("#");
@@ -38,9 +92,6 @@ client.on('connect', function() { //MQTT subscribe
 });
 
 client.on('message', function(topic,message){
-  if (topic =="location"){
-    location = JSON.parse(message)
-  }
   if (topic =="battery"){
     battery = JSON.parse(message);
   }
@@ -50,8 +101,31 @@ client.on('message', function(topic,message){
   
 });
 
+//Routes
 app.get("/battery",(req,res)=>{
-  res.json(battery)
+  let randomNumber = Math.floor(Math.random() * 100);
+
+  const filter = { id: 732 };
+  const update = battery; // global battery varaiable
+
+  Battery.findOneAndUpdate(filter, update, {returnOriginal: false})
+    .then((obj) => res.json(obj))
+    .catch((err) => console.log(err))
+  ;
+
+
+  console.log("Battery Level: ", battery.percentage );
+  // const battery = new Battery({
+  //   percentage: randomNumber,
+  // });
+
+  // battery.save()
+  //   .then((obj) => res.json(obj))
+  //   .catch((err) => console.log(err));
+  
+
+
+  //res.json({percentage:randomNumber})
 })
 
 app.post("/rControl", (req, res) =>{
@@ -62,8 +136,33 @@ app.post("/rControl", (req, res) =>{
 } )
 
 app.get("/coordinates",(req,res)=>{
-  console.log("Server Coords:", location);
-  res.json(location)
+
+  const filter = { id: 732 };
+  const update = location; //global location variable
+
+  Rover.findOneAndUpdate(filter, update, {returnOriginal: false})
+    .then((obj) => res.json(obj))
+    .catch((err) => console.log(err))
+  ;
+
+  // const rover = new Rover({
+  //   xcoord: x,
+  //   ycoord: y,
+  //   obstacle: z
+  // });
+
+  // rover.save()
+  //   .then((obj) => res.json(obj))
+  //   .catch((err) => console.log(err));
+  
+  // let obj = {
+  //   xcoord : x,
+  //   ycoord : y,
+  //   obstacle: z
+  // };
+
+  //console.log("Server Coords:", obj);
+  
 })
 
 app.get("/obstacles",(req,res)=>{
@@ -73,17 +172,29 @@ app.get("/obstacles",(req,res)=>{
   let y2 = Math.floor((Math.random() * 355));
   let colorRandom = colors[ Math.floor(Math.random() * colors.length)]
 
-  let alienObj = {
+
+  const alienObj = new Alien({
     color: colorRandom,
-    xcoorda : x2,
-    ycoorda : y2,
-  };
+    xcoord: x2,
+    ycoord: y2,
+  });
+
+  alienObj.save()
+    .then((obj) => res.json(obj))
+    .catch((err) => console.log(err))
+  ;
+
+
+  // let alienObj = {
+  //   color: colorRandom,
+  //   xcoorda : x2,
+  //   ycoorda : y2,
+  // };
 
   console.log("Server alienObj:", alienObj);
-  res.json(alienObj);
 
 })
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080');
-})
+// app.listen(8080, () => {
+//   console.log('Listening on port 8080');
+// })
