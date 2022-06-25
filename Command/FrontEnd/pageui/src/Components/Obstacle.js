3d0c09169e27e86ec5228d54010859c53843ee24
Aeroking21 <55886814+Aeroking21@users.noreply.github.com>
2022-06-25 10:58:30 +0100

    coorda now is coord

--- Command/FrontEnd/pageui/src/Components/Obstacle.js
+++ Command/FrontEnd/pageui/src/Components/Obstacle.js
@@ -3,12 +3,12 @@
 const Obstacle = (props) => {
   
   return (
-    <div style={{borderRadius:"50%", position:"relative", top: `${props.Alien.xcoorda}px`, left: `${props.Alien.ycoorda}px`, backgroundColor: `${props.color}`, height:'15px', width:'15px' }}>
+    <div style={{borderRadius:"50%", position:"relative", top: `${props.Alien.xcoord}px`, left: `${props.Alien.ycoord}px`, backgroundColor: `${props.color}`, height:'15px', width:'15px' }}>
 
     </div>
   )
 }
 
-//<img src={red} alt={props.Alien.color} style={{border:"4px solid white", position:"relative", top: `${props.Alien.xcoorda}px`, left: `${props.Alien.ycoorda}px`}} />
+
 export default Obstacle
 
